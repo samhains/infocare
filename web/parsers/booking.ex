2365f5395ba9a83f07da78f6167f5d4187aaf755
sdlovecraft <sdh@eml.cc>
2016-08-15 18:10:23 +1000

    got parser to work

--- web/parsers/booking.ex
+++ web/parsers/booking.ex
@@ -5,23 +5,33 @@ defmodule InfoCare.BookingParser do
   require IEx
 
   # {"BookingID":"136892","ChildID":"742","ParentID":"5305","Date":"2016-07-14","StartTime":"07:00","EndTime":"12:40","Absent":"false"}
-  def parse bookings_api_map do
-    bookings_api_map
-    |> Enum.map(&parse_booking/1)
+  def parse bookings_api_map, service do
+    case Map.fetch(bookings_api_map, "Bookings") do
+      {:ok, bookings_data} ->
+        bookings =
+          Enum.map(bookings_data, fn (booking) -> parse_booking(booking, service) end)
+        {:ok, bookings}
+      {:error, error} ->
+        Logger.error("There was a problem fetching 'Bookings' from the Bookings Map received")
+        Logger.error(inspect error)
+    end
   end
 
-  def parse_booking booking_map do
+  def parse_booking booking_map, service do
     date_string = booking_map["Date"]
     date = Timex.parse!(date_string, "%F", :strftime)
     start_time_string = date_string <> booking_map["StartTime"]
-    end_time_string = date_string <> booking_map["EndTime"]
+    end_time_string = date_string <>booking_map["EndTime"]
 
-    start_time = Timex.parse!(start_time_string, "%F%T", :strftime)
-    end_time = Timex.parse!(start_time_string, "%F%T", :strftime)
+    start_time = Timex.parse!(start_time_string, "%F%H:%M", :strftime)
+    end_time = Timex.parse!(end_time_string, "%F%H:%M", :strftime)
 
     %{
       ic_booking_id: booking_map["BookingID"],
-      ic_child_id: booking_map["BookingID"],
+      absent: booking_map["Absent"],
+      ic_child_id: booking_map["ChildID"],
+      ic_parent_id: booking_map["ParentID"],
+      service_id: service.id,
       date: date,
       start_time: start_time,
       end_time: end_time
@@ -32,7 +42,7 @@ defmodule InfoCare.BookingParser do
     case Api.get_bookings_by_service(service, start_date, end_date)  do
       {:ok, bookings_data} ->
         bookings =
-          parse(bookings_data)
+          parse(bookings_data, service)
         {:ok, bookings}
       {:error, error} ->
         Logger.error (inspect error.reason)
